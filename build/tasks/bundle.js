abfc46509966d670ffe756e1c62be61549e4e19c
Jason <jason@questmetrics.com>
2016-07-12 17:38:55 +1000

    Testing example

---
+++ build/tasks/bundle.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var bundler = require('aurelia-bundler');
+var paths = require("../paths");
+
+var bundles = {
+    "aurelia-bundle.min": {
+        "includes": [
+            "aurelia-bootstrapper",
+            "aurelia-dependency-injection",
+            "aurelia-loader",
+            "aurelia-framework",
+            "aurelia-metadata",
+            "aurelia-templating",
+            "aurelia-logging-console",
+            "aurelia-loader-default",
+            "aurelia-templating-binding",
+            "aurelia-templating-router",
+            "aurelia-templating-resources",
+            "aurelia-history-browser",
+            "babel",
+            "core-js",
+            "event-js",
+            "property-resolver",
+            "treacherous"
+        ],
+        "options": {
+            "inject": true,
+            "minify": true,
+            "rev": false
+        }
+    }
+};
+
+var config = {
+    force: true,
+    baseURL: "./examples",
+    configPath: './examples/config.js',
+    bundles: bundles
+};
+
+gulp.task('unbundle:example', function() {
+    return bundler.unbundle(config);
+});
+
+gulp.task('bundle:example', ['build', 'unbundle:example'], function() {
+    return bundler.bundle(config);
+});
